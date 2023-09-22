69430bf99813bf9bfbb17e4db99f755171a62952
Andy Holmes <info@andyholmes.me>
2023-09-22 20:09:47 +0100

    Set up Filament v3 instead

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,12 +19,11 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('name')->virtualAs('concat(first_name, \' \', last_name)');
+            $table->string('avatar_url')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
 
