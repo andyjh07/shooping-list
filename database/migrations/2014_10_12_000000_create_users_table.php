7826c12c0c904c04567b80855c41520f02e93a89
Andy Holmes <info@andyholmes.me>
2023-09-22 18:02:24 +0100

    Set up Jetstream and slightly customised it

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Carbon\Carbon;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,13 +16,25 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('name')->virtualAs('concat(first_name, \' \', last_name)');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
+
+        $andy = User::create([
+            'first_name' => 'Andy',
+            'last_name' => 'Holmes',
+            'email' => 'info@andyholmes.me',
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('password'),
+        ]);
     }
 
     /**
